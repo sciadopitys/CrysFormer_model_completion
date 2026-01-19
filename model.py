4dcb808cfc68f4abc9f4ea2e5022d33eb8f27778
sciadopitys <31415786+sciadopitys@users.noreply.github.com>
2026-01-19 15:40:08 -0600

    Added additional comments to model definition (model.py)

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ import nystrom_ps
 def pair(t):
     return t if isinstance(t, tuple) else (t, t)
 
-
+# Layer normalization of current tokens before feedforward block
 class PreNorm(nn.Module):
     def __init__(self, dim, fn):
         super().__init__()
@@ -20,6 +20,7 @@ class PreNorm(nn.Module):
     def forward(self, x, **kwargs):
         return self.fn(self.norm(x), **kwargs)
 
+# Transformer feedforward block
 class FeedForward(nn.Module):
     def __init__(self, dim, hidden_dim, dropout = 0.):
         super().__init__()
@@ -33,6 +34,7 @@ class FeedForward(nn.Module):
     def forward(self, x):
         return self.net(x)
 
+# Layer normalization of current tokens and partial structure tokens before attention
 class PreNorm_partial_structure(nn.Module):
     def __init__(self, dim, fn):
         super().__init__()
@@ -45,7 +47,10 @@ class PreNorm_partial_structure(nn.Module):
 class Transformer_partial_structure(nn.Module):
     def __init__(self, patch_dim, num_patches, num_patches_ps, patch_height, patch_width, frame_patch_size, num_partial_structure, dim, depth, heads, dim_head, mlp_dim, same_partial_structure_emb=True,dropout = 0., downsample = 0, downsample_by = 0, channels = 10):
         super().__init__()
+
+        # Positional encoding for partial structures
         self.pos_embedding = nn.Parameter(torch.randn(1, num_patches_ps, dim))
+        
         self.layers = nn.ModuleList([])
         self.downsample_by = downsample_by
         self.downupsample = nn.ModuleList([])
@@ -53,12 +58,16 @@ class Transformer_partial_structure(nn.Module):
         self.depth = depth
         self.change_dim = depth // ((downsample + 1) * 2)
         self.same_partial_structure_emb=same_partial_structure_emb
+
         if self.same_partial_structure_emb:
+            # Create transformer blocks of attention block followed by feedforward block
             for _ in range(depth):
                 self.layers.append(nn.ModuleList([
                     PreNorm_partial_structure(dim, nystrom_ps.Nystrom_attention_partial_structure(dim, heads = heads, dim_head = dim_head, num_landmarks = 64, residual = True, dropout = dropout)),
                     PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
                 ]))
+
+            # Single patch embedding for partial structures, used in all transformer layers
             self.partial_structure_to_patch_embedding = nn.Sequential(
                 Rearrange('b c (f pf) (h p1) (w p2) -> b (f h w) (c p1 p2 pf)', p1 = patch_height, p2 = patch_width, pf = frame_patch_size),
                 nn.LayerNorm(patch_dim),
@@ -71,6 +80,8 @@ class Transformer_partial_structure(nn.Module):
                     PreNorm_partial_structure(dim, nystrom_ps.Nystrom_attention_partial_structure(dim, heads = heads, dim_head = dim_head, num_landmarks = 64, residual = True, dropout = dropout)),
                     PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
                 ]))
+
+            # Provide a different partial structure token embedding to each transformer layer
             self.partial_structure_rearrange=Rearrange('b c (f pf) (h p1) (w p2) -> b (f h w) (c p1 p2 pf)', p1 = patch_height, p2 = patch_width, pf = frame_patch_size)
             self.partial_structure_emb_layers=nn.ModuleList([])
             for _ in range(depth):
@@ -82,24 +93,30 @@ class Transformer_partial_structure(nn.Module):
         
         ksize = downsample_by + 1
 
+        # Create downsampling convolution and corresponding upsampling transposed convolution layers
         for _ in range(downsample):
             self.downupsample.append(nn.Conv3d(in_channels=channels, out_channels=channels, kernel_size=ksize, bias=True))
         for _ in range(downsample):
             self.downupsample.append(nn.ConvTranspose3d(in_channels=channels, out_channels=channels, kernel_size=ksize, bias=True))
     def forward(self, x, partial_structure, fsize, hsize, wsize, pfsize, phsize, pwsize):
         if self.same_partial_structure_emb:
-            
+
+            # Create constant partial structure token embedding
             partial_structure = self.partial_structure_to_patch_embedding(partial_structure)
             _,total_partial_patches,_ = partial_structure.shape
             partial_structure = partial_structure + self.pos_embedding[:,:total_partial_patches,:]
+
+            # Counter to determine when to downsample and upsample
             count1 = 1
             count2 = 0
             
             for attn, ff in self.layers:
-                
+
+                # Attention followed by feedforward
                 x = attn(x,partial_structure) + x    
                 x = ff(x) + x
-                
+
+                # After specific transformer blocks rearrange into 3D shape, downsample or upsample, and then rearrange back into a sequence of tokens
                 if ((count1 % self.change_dim) == 0) and (count1 != (self.depth // 2)) and (count1 != self.depth):
                     x = rearrange(x, 'b (f h w) (p1 p2 pf c) -> b c (f pf) (h p1) (w p2)', f=fsize // pfsize, h=hsize // phsize, w=wsize // pwsize, p1 = phsize, p2 = pwsize, pf = pfsize, c=self.channels)
                     x = self.downupsample[count2](x)
@@ -123,8 +140,12 @@ class Transformer_partial_structure(nn.Module):
             count2 = 0
             
             for i in range(len(self.layers)):
+
+                # Block-specific partial structure embeddiing
                 partial_structure_i=self.partial_structure_emb_layers[i](partial_structure)
                 partial_structure_i = partial_structure_i + self.pos_embedding[:,:total_partial_patches,:]
+
+                # Attention followed by feedforward
                 x = self.layers[i][0](x,partial_structure_i) + x
                 x = self.layers[i][1](x) + x
                 
@@ -328,7 +349,7 @@ class ViT_vary_encoder_decoder_partial_structure(nn.Module):
         partial_structure = partial_structure[:,:,scale, :,:,:]
         partial_structure = torch.squeeze(partial_structure, dim=2)
 
-        # Add positional embedding to sequence of Patterson tokens
+        # Apply positional embedding to sequence of Patterson tokens
         x += self.pos_embedding[:, :n]
         x = self.dropout(x)
 
@@ -359,6 +380,7 @@ class ViT_vary_encoder_decoder_partial_structure(nn.Module):
         x= x[:,:,pad_list[4]:(x.shape[2]-pad_list[5]),pad_list[2]:(x.shape[3]-pad_list[3]),pad_list[0]:(x.shape[4]-pad_list[1])]
         
         return(x)
+
 
 
 
